ef911fe5d275af50a7ed6f42d0823a1654bafe1f
Emmett Agnew <eragnew@gmail.com>
2017-04-14 19:30:31 -0700

    Hooked up chart for q19, added lodash to angular client

--- client-angular/static/app.js
+++ client-angular/static/app.js
@@ -15,9 +15,30 @@ var mptSurveys = angular.module('mptSurveys', ['ngRoute', 'chart.js'])
 
     $http.get('http://localhost:4545/api/results/demographics')
       .then(function(response) {
-        console.log(response.data);
+        $scope.loadingData = false;
+        processData(response.data);
       }, function(response){
         console.log(response);
       });
+
+    function processData(data) {
+      console.log(data);
+      var q19 = data.q19;
+      $scope.q19Series = ['Q19'];
+      $scope.q19Labels = [
+        'Under 5',
+        '5 - 9',
+        '10 - 14',
+        '15 - 19',
+        '20 - 24',
+        '25 - 34',
+        '35 - 44',
+        '45 - 54',
+        '55 - 64',
+        '65 - 74',
+        '75+',
+      ];
+      $scope.q19Data = [q19.a1, q19.a2, q19.a3, q19.a4, q19.a5, q19.a6, q19.a7, q19.a8, q19.a9, q19.a10, q19.a11];
+    }
 
   });
